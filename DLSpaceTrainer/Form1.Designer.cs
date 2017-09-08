76035a9146b4057a7191cffea69bf17681297f65
Joe B <joe@megalogamiac.com>
2017-09-08 01:07:14 +0100

    Add ability to lock and unlock levels

--- DLSpaceTrainer/Form1.Designer.cs
+++ DLSpaceTrainer/Form1.Designer.cs
@@ -28,12 +28,153 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnUnlockEarth = new System.Windows.Forms.Button();
+            this.btnUnlockMoon = new System.Windows.Forms.Button();
+            this.btnUnlockMars = new System.Windows.Forms.Button();
+            this.btnUnlockTerra = new System.Windows.Forms.Button();
+            this.btnUnlockFreez = new System.Windows.Forms.Button();
+            this.btnUnlockPyro = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblUnlockLevels = new System.Windows.Forms.Label();
+            this.btnSurfsUp = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.btnUnlockEarth);
+            this.flowLayoutPanel1.Controls.Add(this.btnUnlockMoon);
+            this.flowLayoutPanel1.Controls.Add(this.btnUnlockMars);
+            this.flowLayoutPanel1.Controls.Add(this.btnUnlockTerra);
+            this.flowLayoutPanel1.Controls.Add(this.btnUnlockFreez);
+            this.flowLayoutPanel1.Controls.Add(this.btnUnlockPyro);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(7, 97);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 3, 15, 3);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(134, 181);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // btnUnlockEarth
+            // 
+            this.btnUnlockEarth.Location = new System.Drawing.Point(3, 3);
+            this.btnUnlockEarth.Name = "btnUnlockEarth";
+            this.btnUnlockEarth.Size = new System.Drawing.Size(127, 23);
+            this.btnUnlockEarth.TabIndex = 0;
+            this.btnUnlockEarth.Text = "Unlock Earth";
+            this.btnUnlockEarth.UseVisualStyleBackColor = true;
+            this.btnUnlockEarth.Click += new System.EventHandler(this.btnUnlockEarth_Click);
+            // 
+            // btnUnlockMoon
+            // 
+            this.btnUnlockMoon.Location = new System.Drawing.Point(3, 32);
+            this.btnUnlockMoon.Name = "btnUnlockMoon";
+            this.btnUnlockMoon.Size = new System.Drawing.Size(127, 23);
+            this.btnUnlockMoon.TabIndex = 1;
+            this.btnUnlockMoon.Text = "Unlock Moon";
+            this.btnUnlockMoon.UseVisualStyleBackColor = true;
+            this.btnUnlockMoon.Click += new System.EventHandler(this.btnUnlockMoon_Click);
+            // 
+            // btnUnlockMars
+            // 
+            this.btnUnlockMars.Location = new System.Drawing.Point(3, 61);
+            this.btnUnlockMars.Name = "btnUnlockMars";
+            this.btnUnlockMars.Size = new System.Drawing.Size(127, 23);
+            this.btnUnlockMars.TabIndex = 2;
+            this.btnUnlockMars.Text = "Unlock Mars";
+            this.btnUnlockMars.UseVisualStyleBackColor = true;
+            this.btnUnlockMars.Click += new System.EventHandler(this.btnUnlockMars_Click);
+            // 
+            // btnUnlockTerra
+            // 
+            this.btnUnlockTerra.Location = new System.Drawing.Point(3, 90);
+            this.btnUnlockTerra.Name = "btnUnlockTerra";
+            this.btnUnlockTerra.Size = new System.Drawing.Size(127, 23);
+            this.btnUnlockTerra.TabIndex = 3;
+            this.btnUnlockTerra.Text = "Unlock Terra";
+            this.btnUnlockTerra.UseVisualStyleBackColor = true;
+            this.btnUnlockTerra.Click += new System.EventHandler(this.btnUnlockTerra_Click);
+            // 
+            // btnUnlockFreez
+            // 
+            this.btnUnlockFreez.Location = new System.Drawing.Point(3, 119);
+            this.btnUnlockFreez.Name = "btnUnlockFreez";
+            this.btnUnlockFreez.Size = new System.Drawing.Size(127, 23);
+            this.btnUnlockFreez.TabIndex = 4;
+            this.btnUnlockFreez.Text = "Unlock Freez";
+            this.btnUnlockFreez.UseVisualStyleBackColor = true;
+            this.btnUnlockFreez.Click += new System.EventHandler(this.btnUnlockFreez_Click);
+            // 
+            // btnUnlockPyro
+            // 
+            this.btnUnlockPyro.Location = new System.Drawing.Point(3, 148);
+            this.btnUnlockPyro.Name = "btnUnlockPyro";
+            this.btnUnlockPyro.Size = new System.Drawing.Size(127, 23);
+            this.btnUnlockPyro.TabIndex = 5;
+            this.btnUnlockPyro.Text = "Unlock Pyro";
+            this.btnUnlockPyro.UseVisualStyleBackColor = true;
+            this.btnUnlockPyro.Click += new System.EventHandler(this.btnUnlockPyro_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(12, 13);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(343, 33);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "Duck Life: Space Trainier";
+            // 
+            // lblUnlockLevels
+            // 
+            this.lblUnlockLevels.AutoSize = true;
+            this.lblUnlockLevels.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUnlockLevels.Location = new System.Drawing.Point(8, 70);
+            this.lblUnlockLevels.Name = "lblUnlockLevels";
+            this.lblUnlockLevels.Size = new System.Drawing.Size(127, 24);
+            this.lblUnlockLevels.TabIndex = 0;
+            this.lblUnlockLevels.Text = "Unlock Levels";
+            // 
+            // btnSurfsUp
+            // 
+            this.btnSurfsUp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnSurfsUp.Location = new System.Drawing.Point(7, 341);
+            this.btnSurfsUp.Name = "btnSurfsUp";
+            this.btnSurfsUp.Size = new System.Drawing.Size(75, 23);
+            this.btnSurfsUp.TabIndex = 2;
+            this.btnSurfsUp.Text = "Surfs Up!";
+            this.btnSurfsUp.UseVisualStyleBackColor = true;
+            this.btnSurfsUp.Click += new System.EventHandler(this.btnSurfsUp_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(358, 376);
+            this.Controls.Add(this.btnSurfsUp);
+            this.Controls.Add(this.lblUnlockLevels);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Duck Life Space Trainer";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblUnlockLevels;
+        private System.Windows.Forms.Button btnUnlockEarth;
+        private System.Windows.Forms.Button btnUnlockMoon;
+        private System.Windows.Forms.Button btnUnlockMars;
+        private System.Windows.Forms.Button btnUnlockTerra;
+        private System.Windows.Forms.Button btnUnlockFreez;
+        private System.Windows.Forms.Button btnUnlockPyro;
+        private System.Windows.Forms.Button btnSurfsUp;
     }
 }
 
--- DLSpaceTrainer/Form1.cs
+++ DLSpaceTrainer/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DLSpaceTrainer.Services;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,121 @@ namespace DLSpaceTrainer
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        #region Dependencies
+
+        private readonly IRegistryService _registryService;
+
+        #endregion
+
+        #region Constructor
+
+        public Form1(IRegistryService registryService)
         {
+            _registryService = registryService;
+
             InitializeComponent();
+
+            SetupButtons();
+        }
+
+        #endregion
+
+        #region Event Handlers
+
+        private void btnUnlockEarth_Click(object sender, EventArgs e)
+        {
+            var mapKey = "unlockmap1";
+            handleButton(mapKey);
+        }
+        
+        private void btnUnlockMoon_Click(object sender, EventArgs e)
+        {
+            //unlockmap2_h1723740667
+            var mapKey = "unlockmap2";
+            handleButton(mapKey);
+        }
+
+        private void btnUnlockMars_Click(object sender, EventArgs e)
+        {
+            var mapKey = "unlockmap3";
+            handleButton(mapKey);
+        }
+
+        private void btnUnlockTerra_Click(object sender, EventArgs e)
+        {
+            var mapKey = "unlockmap4";
+            handleButton(mapKey);
+        }
+
+        private void btnUnlockFreez_Click(object sender, EventArgs e)
+        {
+            var mapKey = "unlockmap5";
+            handleButton(mapKey);
+        }
+
+        private void btnUnlockPyro_Click(object sender, EventArgs e)
+        {
+            var mapKey = "unlockmap6";
+            handleButton(mapKey);
+        }
+
+        #endregion
+
+        #region Helpers
+
+        private void SetupButtons()
+        {
+            btnUnlockEarth.Text = "Unlock Earth";
+            btnUnlockMoon.Text = "Unlock Moon";
+            btnUnlockMars.Text = "Unlock Mars";
+            btnUnlockTerra.Text = "Unlock Terra";
+            btnUnlockFreez.Text = "Unlock Freez";
+            btnUnlockPyro.Text = "Unlock Pyro";
+
+            var keys = _registryService.ListValues().Where(x => x.Contains("unlockmap"));
+
+            foreach (var key in keys)
+                SetupButton(key.Split('_')[0]);
+        }
+
+        private void SetupButton(string key)
+        {
+            ///TODO:
+            ///Make this less garbage
+            if (key.Contains("1"))
+                btnUnlockEarth.Text = "Lock Earth";
+            if (key.Contains("2"))
+                btnUnlockMoon.Text = "Lock Moon";
+            if (key.Contains("3"))
+                btnUnlockMars.Text = "Lock Mars";
+            if (key.Contains("4"))
+                btnUnlockTerra.Text = "Lock Terra";
+            if (key.Contains("5"))
+                btnUnlockFreez.Text = "Lock Freez";
+            if (key.Contains("6"))
+                btnUnlockPyro.Text = "Lock Pyro";
+        }
+
+        private void handleButton(string mapKey)
+        {
+            var keys = _registryService.ListValues().Where(x => x.Contains(mapKey));
+
+            if (keys != null && keys.Count() > 0) { 
+                foreach (var key in keys)
+                    if (!_registryService.DeleteKey(key)) 
+                        MessageBox.Show(string.Format("Error locking level {0}", key));
+            }
+            else
+                _registryService.SetKey(mapKey, true);
+
+            SetupButtons();
+        }
+
+        #endregion
+
+        private void btnSurfsUp_Click(object sender, EventArgs e)
+        {
+            _registryService.SetKey("duckData1_costume", 2);
         }
     }
 }
