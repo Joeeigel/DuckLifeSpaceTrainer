aa59052e0b89e0fc1be933802d4ac15adec817e7
Joe B <joe@megalogamiac.com>
2017-09-08 01:07:03 +0100

    Add repository service implimentation

--- DLSpaceTrainer/Services/RepositoryService.cs
+++ DLSpaceTrainer/Services/RepositoryService.cs
@@ -9,28 +9,46 @@ namespace DLSpaceTrainer.Services
 {
     class RegistryService : IRegistryService
     {
-        private RegistryKey regKey = Registry.CurrentUser.OpenSubKey("Software\\Mofunzone\\Duck Life: Space");
+        private RegistryKey regKey = Registry.CurrentUser.OpenSubKey("Software\\Mofunzone\\Duck Life: Space", true);
 
         public Dictionary<string, string> GetKey(string key)
         {
             return new Dictionary<string, string>();
         }
 
-        public void SetKey(string key, string value)
+        public void SetKey(string key, object value)
         {
-            throw new NotImplementedException();
+            var matchingKeys = ListValues().Where(x => x.StartsWith(key));
+
+            foreach (var match in matchingKeys)
+                regKey.DeleteValue(match);
+
+            regKey.SetValue(key, value);
         }
 
         public IList<string> ListValues()
         {
             return regKey.GetValueNames().ToList();
         }
+
+        public bool DeleteKey(string key)
+        {
+            if(regKey.GetValueNames().Any(x => x == key))
+            { 
+                regKey.DeleteValue(key);
+                return true;
+            }
+
+            return false;
+        }
     }
 
     public interface IRegistryService
     {
-        void SetKey(string key, string value);
+        void SetKey(string key, object value);
+        bool DeleteKey(string key);
         Dictionary<string, string> GetKey(string key);
         IList<string> ListValues();
+
     }
 }
